897a895115efe24d2a493363dbec7055422a0a27
Benjamin Skirlo <elektrowolle@users.noreply.github.com>
2022-09-26 13:36:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,18 @@ input.onButtonPressed(Button.A, function () {
     start_round()
 })
 function play () {
-	
+    step = (input.runningTime() - my_turn) / 1000
+    heading = (input.rotation(Rotation.Pitch) + 180) / 360 + 2
+    led.plot(step, heading)
+    if (heading < 0 || heading > 4) {
+        radio.sendString("lost")
+        my_turn = 0
+    } else if (step > 4) {
+        radio.sendValue("now", next_microbit)
+        my_turn = 0
+    } else {
+    	
+    }
 }
 function draw () {
     if (next_turn) {
@@ -22,6 +33,9 @@ function draw () {
             # . . . .
             # . . . .
             `)
+    } else if (my_turn > 0) {
+        basic.clearScreen()
+        play()
     } else {
         led.plot(microbit_list.length, 0)
     }
@@ -41,20 +55,29 @@ radio.onReceivedValue(function (name, value) {
     }
 })
 function start_round () {
-    my_turn = true
-    radio.sendValue("next", microbit_list._pickRandom())
+    my_turn = input.runningTime()
+    next_microbit = microbit_list._pickRandom()
+    next_turn = false
+    radio.sendValue("next", next_microbit)
 }
-let my_turn = false
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    input.calibrateCompass()
+})
+let heading = 0
+let my_turn = 0
 let next_turn = false
+let step = 0
+let next_microbit = 0
 let new_microbit_list: number[] = []
 let microbit_list: number[] = []
 let last_address_check = 0
 let last_advertise = 0
 microbit_list = []
 new_microbit_list = []
-let step = 0
+next_microbit = 0
+step = 0
 next_turn = false
-my_turn = false
+my_turn = 0
 basic.forever(function () {
     if (input.runningTime() > 1000 + last_address_check) {
         address_update()
